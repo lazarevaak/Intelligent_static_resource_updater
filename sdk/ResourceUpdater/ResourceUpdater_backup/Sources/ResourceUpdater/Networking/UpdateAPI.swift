b2e6c41191d1f73fdca1560030cfd1eec3363876
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2026-01-27 00:22:56 +0300

    Remove temporary ResourceUpdater_backup directory

--- sdk/ResourceUpdater/ResourceUpdater_backup/Sources/ResourceUpdater/Networking/UpdateAPI.swift
+++
@@ -1,7 +0,0 @@
-//
-//  UpdateAPI.swift
-//  ResourceUpdater
-//
-//  Created by MacBoock on 26.01.2026.
-//
-
--- sdk/ResourceUpdater/ResourceUpdater_backup/Sources/ResourceUpdater/Storage/LocalResourceStore.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LocalResourceStore.swift
-//  ResourceUpdater
-//
-//  Created by MacBoock on 26.01.2026.
-//
-
