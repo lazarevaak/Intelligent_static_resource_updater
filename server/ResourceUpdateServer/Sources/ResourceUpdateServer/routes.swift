8cb338ea1274279f688aef0d154471bf45f3b87b
karabelnikov.s <karabelnikov.s@rwb.ru>
2026-02-01 22:27:26 +0300

    Создал базовую логику для манифестов

--- server/ResourceUpdateServer/Sources/ResourceUpdateServer/routes.swift
+++ server/ResourceUpdateServer/Sources/ResourceUpdateServer/routes.swift
@@ -1,11 +1,13 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+    let manifestController = ManifestController(publicDirectory: app.directory.publicDirectory)
+
+    app.get { _ in "Серв запущен все работает" }
+
+    app.get("manifest", ":appId", "latest", use: manifestController.getLatestManifest)
+    app.get("manifest", ":appId", "version", ":version", use: manifestController.getManifest)
+    app.get("manifest", ":appId", "versions", use: manifestController.listManifestVersions)
+    app.post("manifest", ":appId", "version", ":version", use: manifestController.updateManifest)
 }
